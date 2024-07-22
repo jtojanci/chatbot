47255c0043dc6acd64a3a325d38ff6818a040141
Juan Pablo Tojanci Duque <jtojanci@sura.com.co>
2024-07-22 16:14:40 -0500

    actualizacion para trabajo en otro compu

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,9 +1,10 @@
-function cambiarCaja(){
-    let boton=document.getElementById("botonChat")
-    let cajaChat=document.getElementById("cajaChat")
-    if(cajaChat.style.display=="none"||cajaChat.style.display=="")
-        cajaChat.style.display="block"
-    else{   
-        cajaChat.style.display="none"
-    }
+let boton=document.getElementById("botonChat")
+
+boton.addEventListener("click",function(){
+  let cajaChat=document.getElementById("cajaChat")
+  if(cajaChat.style.display=="none" || cajaChat.style.display==""){
+    cajaChat.style.display="block"
+  }else{
+    cajaChat.style.display="none"
 }
+})
---
+++ src/controllers/controladorChat.js
@@ -0,0 +1,50 @@
+let botonSend=document.getElementById("botonSend")
+let botonChat=document.getElementById("textoChat")
+let mensajeChat=document.getElementById("mensajeChat")
+
+let preguntas=[
+    "¿Cuál es tu nombre?",
+    "¿Cuántos años tienes",
+    "¿Cuál es tu película favorita",
+    "¿Tienes mascotas?",
+    "¿Cuál es tu hobby?"
+]
+
+let respuestas=[
+    "Hola humano, mi nombre es Firulais",
+    "Tengo 4 años",
+    "Todos los perros van al cielo",
+    "Si, un hamster",
+    "Perseguir ciclistas"
+]
+
+//funcion para iniciar y
+let indicePregunta=0
+
+function procesarEntradaChat(){
+    let escribeUsuario=textoChat.value.toLowerCase()
+    textoChat.value=""
+    if(escribeUsuario=="hola"){
+        let listaPreguntas=preguntas.map((pregunta,index)=>`${index+1}. ${pregunta}`).join("<br>")
+        mensajeChat.innerHTML+=`<p class="text-start">Hola bienvenido <br> ${listaPreguntas}</p>`
+    }
+    else{
+        let numeroPregunta=parseInt(escribeUsuario)-1
+        if(numeroPregunta>=0 && numeroPregunta<preguntas.length){
+            mensajeChat.innerHTML+=`<p class="text-start">${preguntas[numeroPregunta]}</p>`
+            mensajeChat.innerHTML+=`<p class="text-end">${respuestas[numeroPregunta]}</p>`
+        }
+    }
+}
+
+//rutina para activar el chatbot
+
+botonSend.addEventListener("click",procesarEntradaChat)
+
+textoChat.addEventListener("keypress",function(evento){
+    if(evento.key=="Enter"){
+        evento.preventDefault()
+        procesarEntradaChat()
+    }
+
+})
